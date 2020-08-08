f7316cf7b35aa4f8bae81508f7c5e9fd8846d5b5
Frank Hulmes <fdhulmes@yahoo.com>
2020-08-08 18:23:27 -0400

    Created all classes, interfaces, and enum for the weekday routines

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/weekdayRoutine/SundayRoutine.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington.froilansfarm.weekdayRoutine;
+
+import com.zipcodewilmington.froilansfarm.farm.Farm;
+import com.zipcodewilmington.froilansfarm.farm.Farmer;
+
+public class SundayRoutine implements WeekdayRoutine {
+    public void doRoutine(Farm farm, Farmer farmer) {
+
+    }
+
+    public void farmerRoutine(Farm farm, Farmer farmer) {
+
+    }
+
+    public void PilotRoutine(Farm farm, Farmer farmer) {
+
+    }
+}
