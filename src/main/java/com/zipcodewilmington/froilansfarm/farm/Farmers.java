5ea8c46b0ba5ba2bde5ff5f6c2465bc9b11f97ad
Frank Hulmes <fdhulmes@yahoo.com>
2020-08-08 12:09:51 -0400

    fleshed out getters for Person, set up Farmers singleton

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/Farmers.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/Farmers.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Farmers {
@@ -8,6 +9,16 @@ public class Farmers {
     private List<Farmer> farmerList;
 
     private Farmers(){
+        this.farmerList = new ArrayList<Farmer>();
+        this.farmerList.add(new Farmer("Froilan", 1));
+        this.farmerList.add(new Pilot("Froilanda", 2));
+    }
 
+    public Farmers getInstance(){
+        return this.instance;
+    }
+
+    public List<Farmer> getFarmerList(){
+        return this.farmerList;
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/farm/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/Person.java
@@ -8,6 +8,17 @@ public abstract class Person implements Eater, NoiseMaker {
     protected Integer id;
 
     public Person(String name, Integer id){
-
+        this.name = name;
+        this.id = id;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+
 }
