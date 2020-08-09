7ff9e2907460462d75fd683cfce64f9cf15c5dc6
Julie Moon <juliemoon95@gmail.com>
2020-08-08 20:24:53 -0400

    added vehicle classes and method signatures

--- src/main/java/com/zipcodewilmington/froilansfarm/farm/Aircraft.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/Aircraft.java
@@ -1,4 +1,9 @@
 package com.zipcodewilmington.froilansfarm.farm;
 
-public abstract class Aircraft extends Vehicle{
+public abstract class Aircraft extends Vehicle {
+
+    public void fly() {
+
+    }
+
 }
---
+++ src/main/java/com/zipcodewilmington/froilansfarm/farm/CropDuster.java
@@ -0,0 +1,22 @@
+package com.zipcodewilmington.froilansfarm.farm;
+
+import com.zipcodewilmington.froilansfarm.interfaces.FarmVehicle;
+
+public class CropDuster extends Aircraft implements FarmVehicle {
+
+    public void operate(Farm farm) {
+
+    }
+
+    public String makeNoise() {
+        return null;
+    }
+
+    public void ride() {
+
+    }
+
+    public void fertilize(CropRow row) {
+
+    }
+}
