9c6f2964b52e9284cb49d1a6e0da052ce459c827
Frank Hulmes <fdhulmes@yahoo.com>
2020-08-10 20:39:23 -0400

    Sunday routine

--- src/main/java/com/zipcodewilmington/froilansfarm/weekdayRoutine/SundayRoutine.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/weekdayRoutine/SundayRoutine.java
@@ -6,42 +6,17 @@ import java.util.List;
 
 public class SundayRoutine implements WeekdayRoutine {
 
-    private Farm farm;
-    private Farmers farmers = Farmers.getInstance();
-    private Object EarCorn;
-
     public void doRoutine(Farm farm, Farmer farmer) {
-        farmerRoutine(farm, farmers.getFarmerList().get(0));
-        pilotRoutine(farm, farmers.getFarmerList().get(1));
-    }
-
-    public void farmerRoutine(Farm farm, Farmer farmer) {
-        farmer.eatBreakfast(farm.getEdibleInventory());
-        for(Stable stable : farm.getStables()) {
-            for(Horse horse : stable.getInhabitants()) {
-                horse.ride();
-                horse.feedHorse((Inventory) EarCorn);
-            }
-        }
-        List<CropRow> cropRows = farm.getField().getField();
-            for(int i = 1; i <= 100; i++) {
+        if(farmer.getName().equals("Froilan")) {
+            List<CropRow> cropRows = farm.getCompleteField().getField();
+            for (int i = 1; i <= 100; i++) {
                 farmer.plant(new CornStalk(), cropRows.get(0));
             }
-            for(int i = 1; i <= 20; i++) {
+            for (int i = 1; i <= 20; i++) {
                 farmer.plant(new TomatoPlant(), cropRows.get(1));
             }
-            for(int i = 1; i <= 40; i++) {
+            for (int i = 1; i <= 40; i++) {
                 farmer.plant(new WheatPlant(), cropRows.get(2));
-            }
-
-    }
-
-    public void pilotRoutine(Farm farm, Farmer farmer) {
-        farmer.eatBreakfast(farm.getEdibleInventory());
-        for(Stable stable : farm.getStables()) {
-            for(Horse horse : stable.getInhabitants()) {
-                horse.ride();
-                horse.feedHorse((Inventory) EarCorn);
             }
         }
     }
